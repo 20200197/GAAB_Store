e536a75f1e507e05d62553b614ba60dfcf626254
20200197 <84478171+20200197@users.noreply.github.com>
2022-03-03 15:42:41 -0600

    Add files via upload

---
+++ GAAB_Store/js/sitio_privado/estadisticas.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function() {
+    M.Sidenav.init(document.querySelectorAll('.sidenav'));
+    M.Slider.init(document.querySelectorAll('.slider'));
+    M.Carousel.init(document.querySelectorAll('.carousel'));
+  });
+
+
+
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.materialboxed');
+  var instances = M.Materialbox.init(elems);
+});
+
