57731f0de16608cdd812d3f822d3d4f9e45f7421
20200197 <84478171+20200197@users.noreply.github.com>
2022-04-29 17:46:58 -0600

    Add files via upload

---
+++ GAAB_Store/controladores/dashboard/adminClientes.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function() {
+    M.Sidenav.init(document.querySelectorAll('.sidenav'));
+    M.Slider.init(document.querySelectorAll('.slider'));
+    M.Carousel.init(document.querySelectorAll('.carousel'));
+    M.Modal.init(document.querySelectorAll('.modal'));
+  });
+
+
+
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems);
+});
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.dropdown-trigger');
+  let options={
+    alignment:'left'
+    
+  };
+  var instances = M.Dropdown.init(elems,options);
+});
